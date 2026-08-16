32e426bd2a45d6f85823a9c19405f0e7802d0af8
Ziddan <ziddanm45@gmail.com>
2026-08-16 19:14:49 +0700

    menambahkan logo

--- public/admin/template/header.php
+++ public/admin/template/header.php
@@ -1,14 +1,43 @@
 <header id="header" class="position-sticky d-flex flex-wrap align-items-center justify-content-center justify-content-md-between border-bottom bg-primary">
-    <a href="../dashboard" class="d-flex align-items-center col-md-3 mb-2 mb-md-0  text-dark text-decoration-none">
+    <a href="../dashboard" class="d-flex align-items-center col-md-3 mb-2 mb-md-0  text-dark text-decoration-none" style="pointer-events: none;">
         <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
             <use xlink:href="#bootstrap" />
         </svg>
         <!-- <span>Logo Koperasi</span> -->
-        <img src="../../assets/img/img/logo.jpg" alt="Logo Koperasi" srcset="" width="50" height="50">
+        <!-- <img src="../../assets/img/img/logo.png" alt="Logo Koperasi" srcset="" width="50" height="50"> -->
+
+        <div class="d-flex fw-bold align-items-center">
+            <svg viewBox="0 0 16 16" width="48" height="48" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8,0.71 L14.59,5.29 L13.11,5.29 L13.11,11.41 L8,14.71 L2.89,11.41 L2.89,5.29 L1.41,5.29 Z"
+                    fill="none" stroke="#2E9E44" stroke-width="0.61" stroke-linejoin="round" stroke-linecap="round" />
+
+                <polygon points="4.12,6.35 8,3.53 11.88,6.35 10.47,6.35 8,4.82 5.53,6.35"
+                    fill="#7CC13B" />
+
+                <polygon points="5.29,7 8,5.5 10.71,7 10.71,12 5.29,12"
+                    fill="#1B4C96" />
+
+                <path d="M6.35,7.82 L7.06,7.82 L7.29,8.29 L9.65,8.29 L9.29,9.71 L7.18,9.71 Z"
+                    fill="none" stroke="#FFFFFF" stroke-width="0.24" stroke-linejoin="round" stroke-linecap="round" />
+                <line x1="7.8" y1="8.2" x2="7.8" y2="9.6" stroke="#FFFFFF" stroke-width="0.19" />
+                <line x1="8.4" y1="8.2" x2="8.4" y2="9.6" stroke="#FFFFFF" stroke-width="0.19" />
+                <line x1="9" y1="8.2" x2="9" y2="9.6" stroke="#FFFFFF" stroke-width="0.19" />
+
+                <line x1="7" y1="9.5" x2="7.8" y2="10.1" stroke="#FFFFFF" stroke-width="0.25" />
+                <line x1="7.8" y1="10.1" x2="9.3" y2="10.1" stroke="#FFFFFF" stroke-width="0.25" />
+
+                <circle cx="7.65" cy="10.4" r="0.33" fill="#FFFFFF" />
+                <circle cx="9.06" cy="10.4" r="0.33" fill="#FFFFFF" />
+            </svg>
+            <div class="d-flex flex-column">
+                <span class="text-info" style="font-size: 1rem;">ASSACOM</span>
+                <span class="" style="font-size: .75rem; color: greenyellow">KOPERASI SEKOLAH</span>
+            </div>
+        </div>
     </a>
 
     <div class=" w-50 d-flex" style="margin-left: 12em;">
-        <h2 class="">ASSACOM</h2>
+        <h2 class="text-info">ASSACOM</h2>
     </div>
 
     <a href="../profile" class="me-5">
